632c132edb5df999bb27d60c51e8946bc65c4744
Felix Chan <felixchan29@gmail.com>
2018-07-21 15:30:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
   reverse_sentence = sentence.split(" ")
-  reverse_sentence.collect do |string| string.reverse
-  end
+  reverse_sentence.collect {|string| string.reverse}
   reverse_sentence.join(" ")
 end
